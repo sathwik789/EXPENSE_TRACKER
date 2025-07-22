53bcc2a937f087309b2823d0d5998451ca763ba4
Sathwik Reddy <reddysathwik019@gmail.com>
2025-07-22 11:37:25 +0530

    eight2 commit

--- client/expense-tracker/src/utils/axiosinstance.js
+++ client/expense-tracker/src/utils/axiosinstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { BASE_URL } from "./apiPaths";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + '/api/v1',
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
