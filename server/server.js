9f086a0d3173ce5e9ca80fd1620238dc9e7e5970
Sathwik Reddy <reddysathwik019@gmail.com>
2025-07-22 10:48:06 +0530

    fourth commit

--- server/server.js
+++ server/server.js
@@ -1,24 +1,28 @@
 require("dotenv").config();
-const express  = require("express");
+const express = require("express");
 const cors = require("cors");
-const path = require("path")
-const connectDB  = require("./config/db")
+const path = require("path");
+const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const incomeRoutes = require("./routes/incomeRoutes");
 const expenseRoutes = require("./routes/expenseRoutes");
 const dashboardRoutes = require('./routes/dashboardRoutes');
+
 const app = express();
 
-//Middleware to handle cors
+// Connect to MongoDB
+connectDB();
+
+// CORS setup
 const allowedOrigins = [process.env.CLIENT_URL, "http://localhost:5173"];
 
 app.use(cors({
   origin: function (origin, callback) {
-    console.log("Incoming origin :",origin);
+    console.log("Incoming origin:", origin);
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      console.log("Blocked by CORS:",origin);
+      console.log("Blocked by CORS:", origin);
       callback(new Error("Not allowed by CORS"));
     }
   },
@@ -27,19 +31,17 @@ app.use(cors({
   credentials: true,
 }));
 
-
+// Middleware
 app.use(express.json());
 
-connectDB();
-
+// Routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/income", incomeRoutes);
 app.use("/api/v1/expense", expenseRoutes);
 app.use("/api/v1/dashboard", dashboardRoutes);
 
+// Static uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-
-//Server Uploads folder
-app.use("/uploads",express.static(path.join(__dirname,"uploads")));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
