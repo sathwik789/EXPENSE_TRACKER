b5cce159c14abd8d47cff8c43aedc9d320098f8e
Sathwik Reddy <reddysathwik019@gmail.com>
2025-07-22 11:42:55 +0530

    ninth commit

--- client/expense-tracker/src/utils/apiPaths.js
+++ client/expense-tracker/src/utils/apiPaths.js
@@ -4,7 +4,7 @@ export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 //utils/apipaths.js
 export const API_PATHS = {
     AUTH :{
-        LOGIN : "/api/v1/auth/login",
+        LOGIN : "auth/login",
         REGISTER : "/api/v1/auth/register",
         GET_USER_INFO : "/api/v1/auth/getUser",
     },
