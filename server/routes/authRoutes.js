db3779daaaef91793c3c341078fdbb29756af678
Sathwik Reddy <reddysathwik019@gmail.com>
2025-07-22 11:10:28 +0530

    sixth commit

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -13,12 +13,14 @@ router.post("/register",registerUser);
 router.post("/login",loginUser);
 router.post("/verify-otp", verifyOtp);
 router.get("/getUser", protect, getUserInfo);
-router.post("/upload-image",upload.single("image"),(req,res) => {
-    if(!req.file){
-        return res.status(400).json({message:" No file uploaded"});
+router.post("/upload-image", upload.single("image"), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ message: "No file uploaded" });
     }
-    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
 
-    res.status(200).json({imageUrl});
+    const imageUrl = `${process.env.BACKEND_URL}/uploads/${req.file.filename}`;
+
+    res.status(200).json({ imageUrl });
 });
+
 module.exports = router;
